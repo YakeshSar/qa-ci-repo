20f2c6f3396feb6cff56ae66ab28034a5bc1e251
Yakeshkumar-1507 <yakeshsiva1507@outlook.com>
2026-06-05 13:59:45 +0530

    launch google

---
+++ src/test/java/org/scalesoft/AppTest.java
@@ -0,0 +1,26 @@
+package org.scalesoft;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class AppTest
+{
+
+
+    @Test
+    public  void launch() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/");
+        System.out.println("successfully application launched");
+        driver.close();
+
+    }
+}
